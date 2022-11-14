eae31db750facfa81de6fbb5be3816f323516f72
NomanKhan3701 <noman.khan3701@gmail.com>
2022-11-14 09:17:49 +0530

    Added review in navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,25 +5,24 @@ import SearchExercises from '../components/SearchExercises';
 import Exercises from '../components/Exercises';
 
 const Home = () => {
-  // const [bodyPart, setBodyPart] = useState('all');
-  // const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
 
-  // return (
-  //   <Box>
-  //     <HeroBanner />
-  //     <SearchExercises 
-  //       setExercises={setExercises}
-  //       bodyPart={bodyPart}
-  //       setBodyPart={setBodyPart}
-  //     />
-  //     <Exercises 
-  //       exercises ={exercises}
-  //       setExercises={setExercises}
-  //       bodyPart={bodyPart}
-  //     />
-  //   </Box>
-  // )
-  return null;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
+    </Box>
+  )
 }
 
 export default Home
